3be2885f5e8ea5bb1c1f9347be3b07c35bc52666
Algot Sarika <sarika@joindevops.com>
2025-03-14 14:45:53 +0530

    terraform-vpc-module

--- vpc.tf
+++ vpc.tf
@@ -5,4 +5,8 @@ module "vpc" {
     vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
+    public_subnet_cidirs = var.public_subnet_cidirs
+    private_subnet_cidirs = var.private_subnet_cidirs
+    database_subnet_cidirs = var.database_subnet_cidirs
+    is_peering_required = true
 }
